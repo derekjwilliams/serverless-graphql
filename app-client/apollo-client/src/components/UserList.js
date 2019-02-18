213345363db2edddea72267cd1bd36cadd9164ce
Derek Williams <derek61@gmail.com>
2019-02-18 16:55:12 -0700

    updated to latest packages, fixed production absolute path (/static) issue

--- app-client/apollo-client/src/components/UserList.js
+++ app-client/apollo-client/src/components/UserList.js
@@ -57,6 +57,6 @@ export default graphql(UserQuery, {
       consumer_key: process.env.REACT_APP_CONSUMER_KEY,
       consumer_secret: process.env.REACT_APP_SECRET_KEY,
     },
-    pollInterval: 10000,
+    pollInterval: 300000,
   }),
 })(UserList);
